d0f2108a2215b567185f9caeb73d9ef340d4f29b
Roma3577 <mularr37@gmail.com>
2025-02-15 17:05:37 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const div = document.querySelector(".widget");
+const span = document.querySelector(".color");
+const chang = document.querySelector(".change-color");
+const body = document.body;
+chang.addEventListener("click", handleClick);
+function handleClick(event) {
+const newColor = getRandomHexColor(); 
+  body.style.backgroundColor = newColor; 
+  span.textContent = newColor
+}
+
