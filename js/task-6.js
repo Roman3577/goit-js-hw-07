936108d325757b3ee38225127c352eba70452a64
Roma3577 <mularr37@gmail.com>
2025-02-15 21:55:16 +0200

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,35 @@
-function getRandomHexColor() {
+ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
+const controls = document.getElementById("controls");
+const boxes = document.getElementById("boxes");
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+let number = 0; 
+controls.addEventListener("input", (event) => {
+  number = event.target.value.trim();
+});
+function createBoxes(amount) {
+  let size = 30 + boxes.children.length * 10; 
+  for (let i = 0; i < amount; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${size}px`;
+    square.style.height = `${size}px`;
+    square.style.backgroundColor = getRandomHexColor();
+    boxes.append(square);
+    size += 10; 
+  }
+}
+create.addEventListener("click", () => {
+  if (number !== "" && number > 0 && number <= 100) {
+    createBoxes(Number(number));
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
+});
+destroy.addEventListener("click", () => {
+  boxes.innerHTML = ""; 
+});
